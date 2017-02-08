099bfc45a6c17280be0c0bfd3c8c2abe57ef36b1
Connor-Marble <connor@packer-debian-8-amd64.droplet.local>
2017-02-08 03:57:04 +0000

    Completed Json Dumping

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,8 +1,12 @@
+import os
+import time
 import re
 from sys import argv
 import urllib2
 from xml.etree import ElementTree as et
 from collections import namedtuple
+
+destination = './reviews'
 
 #request string
 url_base = "https://steamcommunity.com/app/{appid}/homecontent/?userreviewsoffset={offset}&p={pagenum}&browsefilter=mostrecent&appHubSubSection=10"
@@ -12,13 +16,15 @@ content_start = '<div class="apphub_UserReviewCardContent">'
 content_end = '<div class="UserReviewCardContent_Footer">'
 
 date_pattern=re.compile(r'<div class="date_posted">Posted:*(?P<date>.+)*</div>')
-thumbs_up_pattern=re.compile('icon_thumbsUp.png')
+thumbs_up_pattern='thumbsUp.png'
 found_helpful_pattern=re.compile('(?P<count>[0-9]{1,4}) of [0-9]{1,4} people \((?P<percent>[0-9]{1,2})\%\) found this review helpful')
 
+title_pattern=re.compile('<div class="apphub_AppName">(?P<title>.*?)</div>')
+
+store_url_base="http://store.steampowered.com/app/{}/"
 
 #rview structure
 Review=namedtuple('Review', 'date text thumbs_up foundhelpful helpfulratio')
-
 
 class ReviewScraper(object):
 
@@ -27,6 +33,7 @@ class ReviewScraper(object):
         self.ratelimit=ratelimit
         self.page=0
         self.currentpage=[]
+        self.title=None
 
     def __iter__(self):
         return self
@@ -38,7 +45,7 @@ class ReviewScraper(object):
 
     def __exit__(self, error_type, error_value, traceback):
         #where we will record our place in DB
-        print error_type
+        print error_type or ''
         return not bool(error_type or error_value or traceback)
 
     def _get_next_page(self):
@@ -48,6 +55,15 @@ class ReviewScraper(object):
         self.page += 1
         return bool(self.currentpage)
 
+    def get_title(self):
+        if not self.title:
+            request_url=store_url_base.format(self.gameid)
+            storepage=urllib2.urlopen(request_url).read()
+            title_m=re.search(title_pattern ,storepage)
+            self.title = title_m.group('title')
+
+        return self.title
+    
     def next(self):
         if self.currentpage:
             return self.currentpage.pop(0)
@@ -77,7 +93,7 @@ class ReviewScraper(object):
                         helpful_count=int(found_helpful_m.group('count'))
                         helpful_ratio=float(found_helpful_m.group('percent'))/100.0
 
-                    thumbs_up=bool(re.search(thumbs_up_pattern, content))
+                    thumbs_up=bool(thumbs_up_pattern in content)
                     date = re.search(date_pattern, content).group('date')
                     
                     content=clean_review_text(content)
@@ -94,21 +110,31 @@ class ReviewScraper(object):
 
             return output
 
-def parse_review(text):
-    output = []
-    start = 0
-    try:
-        while(True):
-            start = text.index(content_start, start+len(content_start)-1)
-            end = text.index(content_end, start)
-            output.append(text[start:text.rindex('</div>', start, end)-10])
 
-    except:
-        pass
-    
-    return map(clean_review_text, output)
-    
+def dump_reviews_to_json(gameid, count):
 
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+        
+    
+    with ReviewScraper(gameid) as scraper:
+        with open('{}/{}.json'.format(destination, gameid), 'w') as output:
+            
+            output.write(scraper.get_title() + '\n')
+            
+            for i, review in enumerate(scraper):
+                json = ('{{"num_found_helpful": {},'+\
+                          '"num_found_unhelpful": {},'+\
+                          '"rating": {},'+\
+                          '"review":{}}}\n').format(review.foundhelpful,
+                                                review.foundhelpful*(1-review.helpfulratio),
+                                                review.thumbs_up,
+                                                review.text)
+                output.write(json)
+                if i > count:
+                    break
+
+        
 def clean_review_text(review_text):
     if '</div>' in review_text:
         return review_text[review_text.rindex('</div>')+7:].strip()
@@ -118,11 +144,8 @@ if __name__=='__main__':
 
     gameid = int(input('>Gameid? '))
     count = int(input('reviews to fetch?'))
-    
-    with ReviewScraper(gameid) as scraper:
-        print scraper
-        for i, review in enumerate(scraper):
-            print review
-            print '-'*80
-            if i is count:
-                break
+
+    start = time.time()
+
+    dump_reviews_to_json(gameid, count)
+
